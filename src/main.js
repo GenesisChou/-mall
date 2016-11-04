bda56e04efd1d0f99cd36b42f743c4f4e3e110e7
GenesisChou <447074838@qq.com>
2016-11-04 10:23:53 +0800

    线上测试1

--- src/main.js
+++ src/main.js
@@ -4,10 +4,12 @@ import VueResource from 'vue-resource'
 import router from './router.js'
 import utils from 'libs/utils.js'
 import store from 'v_vuex/store.js'
-
+import FastClick from 'fastclick'
+import wx from 'weixin-js-sdk'
+import wxConfig from './wx_config'
 window.APP = {
     APP_NAME: '积分兑换',
-    HOST: 'http://integral.api.justtong.com/imall', //接口域名
+    HOST: 'http://test.integral.api.justtong.com/imall', //接口域名
     MALL_HOST: 'http://test.imall.justtong.com', //服务器域名
     SUCCESS: 10000, //服务端返回成功状态码
     PERPAGE: 20, //分页查询时每页条数
@@ -25,7 +27,9 @@ if (!utils.getParameterByName('token')) {
     window.APP.USER_ID = utils.getParameterByName('userid');
     window.APP.HEAD_IMG = utils.getParameterByName('headimg');
     window.APP.MEDIA_ID = utils.getParameterByName('mediaid');
-    // wxConfig(wx);
+    FastClick.attach(document.body);
+    //配置微信jsdk
+    wxConfig(wx,store);
     Vue.use(VueResource);
     Vue.http.options.emulateJSON = true; //设置vue-resource post请求参数类型为formdata
     new Vue({
