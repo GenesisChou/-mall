d2e337468e8c54b426a0bf4d47aa51a1f5946f36
genesischou <447074838@qq.com>
2016-12-15 18:29:59 +0800

    build control

--- src/router.js
+++ src/router.js
@@ -8,60 +8,51 @@ const router = new VueRouter({
     routes: [{
             path: '/',
             name: 'index',
-            component: function(resolve) {
-                require(['views/index.vue'], resolve);
-            }
+            component: require('views/index.vue')
+
         }, {
             path: '/index',
             name: 'index',
-            component: function(resolve) {
-                require(['views/index.vue'], resolve);
-            }
+            component: require('views/index.vue')
+
         }, {
             path: '/product_list',
             name: 'product_list',
-            component: function(resolve) {
-                require(['views/product_list.vue'], resolve);
-            }
+            component: require('views/product_list.vue')
+
         }, {
             path: '/product_detail',
             name: 'product_detail',
-            component: function(resolve) {
-                require(['views/product_detail.vue'], resolve);
-            }
+            component: require('views/product_detail.vue')
+
         },
         {
             path: '/activity_detail',
             name: 'activity_detail',
-            component: function(resolve) {
-                require(['views/activity_detail.vue'], resolve);
-            }
+            component: require('views/activity_detail.vue')
+
         }, {
             path: '/my_integral',
             name: 'my_integral',
-            component: function(resolve) {
-                require(['views/my_integral.vue'], resolve);
-            }
+            component: require('views/my_integral.vue')
+
         }, {
             path: '/order_list',
             name: 'order_list',
-            component: function(resolve) {
-                require(['views/order_list.vue'], resolve);
-            }
+            component: require('views/order_list.vue')
+
         }, {
             path: '/order_detail',
             name: 'order_detail',
-            component: function(resolve) {
-                require(['views/order_detail.vue'], resolve);
-            }
+            component: require('views/order_detail.vue')
         },
-         {
-            path:'/test',
-            name:'test',
-            component:function(resolve){
-                require(['views/test.vue'],resolve);
-            }
-        },
+        //  {
+        //     path:'/test',
+        //     name:'test',
+        //     component:function(resolve){
+        //         require(['views/test.vue'],resolve);
+        //     }
+        // },
 
         { path: '*', redirect: '/' }
     ],
