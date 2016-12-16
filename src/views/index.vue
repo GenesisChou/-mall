6e4a3c322bbcf562f17838b254eb4eab4c87d41b
genesischou <447074838@qq.com>
2016-12-16 19:42:07 +0800

    路由切换 scroll事件重启

--- src/views/index.vue
+++ src/views/index.vue
@@ -132,6 +132,7 @@ export default {
     activated(){
       var position=utils.getSessionStorage('position:'+this.$route.name);
       window.scrollTo(0,position);
+      window.addEventListener('scroll',this.getScrollData);
     },
     data() {
         return {
