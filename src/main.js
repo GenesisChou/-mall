4fd147cd52c3c36d0f877d9dfbc0b45bfaf93772
genesischou <447074838@qq.com>
2016-12-19 13:55:11 +0800

    回滚

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 import utils from 'libs/utils.js';
 window.APP = {
     TITLE: '积分兑换',
-    // APPID: 'wx8057c4704888d230',
-    // HOST: 'http://integral.api.justtong.com/imall', //接口域名
-    // MALL_HOST: 'http://imall.justtong.com', //服务器域名
-    APPID: 'wxda819741c7aa5b47',
-    HOST: 'http://test.integral.api.justtong.com/imall', //接口域名
-    MALL_HOST: 'http://test.imall.justtong.com', //服务器域名
+    APPID: 'wx8057c4704888d230',
+    HOST: 'http://integral.api.justtong.com/imall', //接口域名
+    MALL_HOST: 'http://imall.justtong.com', //服务器域名
+    // APPID: 'wxda819741c7aa5b47',
+    // HOST: 'http://test.integral.api.justtong.com/imall', //接口域名
+    // MALL_HOST: 'http://test.imall.justtong.com', //服务器域名
     LOGO: 'http://static.justtong.com/uploads/images/goods/20161025/142505580efac1ad494.jpg',
     SUCCESS: 10000, //服务端返回成功状态码
     PERPAGE: 20, //分页查询时每页条数
@@ -26,26 +26,46 @@ if (token) {
         DATE: new Date()
     };
     utils.setLocalStorage(media_id, cache);
-    // var link = `${APP.MALL_HOST}/?id=${media_id}`;
-    // if (activity_id) {
-    //     link = link + '#/activity_detail?activity_id=' + activity_id;
-    // }
-    // location.href = link;
-    startApp(cache);
+    var link = `${APP.MALL_HOST}/?id=${media_id}`;
+    if (activity_id) {
+        link = link + '#/activity_detail?activity_id=' + activity_id;
+    }
+    location.href = link;
 } else {
     var media_id = utils.getParameterByName('id');
     var cache = utils.getLocalStorage(media_id);
-    console.log(cache);
     //无缓存
     if (!cache) {
         wxLogin(media_id,activity_id);
-        console.log('no cache');
     //缓存过期
     } else if (cacheExpire(cache)) {
         wxLogin(media_id,activity_id);
-        console.log('out of date');
     } else {
-      startApp(cache);
+        var Vue = require('vue');
+        var VueResource = require('vue-resource');
+        var store = require('./vuex/store.js');
+        var FastClick = require('fastclick');
+        var wxConfig = require('./wx_config');
+        console.log('login success');
+        APP.TOKEN = cache.TOKEN;
+        APP.USER_ID = cache.USER_ID;
+        APP.MEDIA_ID = cache.MEDIA_ID;
+        APP.OPEN_ID = cache.OPEN_ID;
+        var title = utils.getParameterByName('imall_title');
+        if (title) {
+            window.APP.TITLE = title;
+        }
+        utils.setTitle(window.APP.TITLE);
+        FastClick.attach(document.body);
+        Vue.use(VueResource);
+        Vue.http.options.emulateJSON = true; //设置vue-resource post请求参数类型为formdata
+        wxConfig(Vue);
+        new Vue({
+            el: '#app',
+            render: h => h(require('./APP.vue')),
+            router: require('./router.js'),
+            store
+        });
     }
 }
 //判断是否过期
@@ -64,32 +84,4 @@ function wxLogin(media_id,activity_id) {
         link = link + '&activity_id=' + activity_id;
     }
     location.href = link;
-}
-function startApp(cache){
-  var Vue = require('vue');
-  var VueResource = require('vue-resource');
-  var store = require('./vuex/store.js');
-  var FastClick = require('fastclick');
-  var wxConfig = require('./wx_config');
-  console.log('login success');
-  APP.TOKEN = cache.TOKEN;
-  APP.USER_ID = cache.USER_ID;
-  APP.MEDIA_ID = cache.MEDIA_ID;
-  APP.OPEN_ID = cache.OPEN_ID;
-  var title = utils.getParameterByName('imall_title');
-  if (title) {
-      window.APP.TITLE = title;
-  }
-  utils.setTitle(window.APP.TITLE);
-  FastClick.attach(document.body);
-  Vue.use(VueResource);
-  Vue.http.options.emulateJSON = true; //设置vue-resource post请求参数类型为formdata
-  wxConfig(Vue);
-  new Vue({
-      el: '#app',
-      render: h => h(require('./APP.vue')),
-      router: require('./router.js'),
-      store
-  });
-
 }
