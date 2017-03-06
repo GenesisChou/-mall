b3e55f54def46f8e7c52152cce03e598ed3a414b
genesischou <447074838@qq.com>
2017-03-06 16:16:28 +0800

    地址添加左边距

--- src/views/orderDetail/orderDetail.vue
+++ src/views/orderDetail/orderDetail.vue
@@ -25,7 +25,6 @@
         background-repeat: no-repeat;
         .location {
             width: pxTorem(73);
-            padding-left: pxTorem(30);
             .iconfont {
                 font-size: pxTorem(30);
             }
@@ -37,10 +36,10 @@
             -webkit-align-items: center;
             height: pxTorem(157);
             margin-top: pxTorem(20);
+            padding:0 pxTorem(30);
             .address-content {
                 flex: 1;
                 -webkit-flex: 1;
-                padding-right: pxTorem(30);
                 overflow: hidden;
             }
             .address-detail {
