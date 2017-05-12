e3950d5c1cdc8937cd8459eea576a32eeabb00fe
genesischou <447074838@qq.com>
2017-05-12 13:42:07 +0800

    - 橱窗位顺序替换

--- src/views/index/components/vSubjectOne.vue
+++ src/views/index/components/vSubjectOne.vue
@@ -1,31 +1,23 @@
 <style lang='scss' scoped>
     @import '../../../assets/scss/variable.scss';
     .v-subject {
-        margin-bottom:pxTorem(14);
-        overflow: hidden;
-        background-color: $white;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        height: pxTorem(252);
+        padding: 0 pxTorem(28); 
+        margin-bottom: pxTorem(14);
+        background-color: $white; 
         border-bottom: 1px solid #d3d4d6;
-        img:nth-child(1) {
-            width: pxTorem(456);
-            height: pxTorem(456);
-            margin: pxTorem(14) 0 pxTorem(14) pxTorem(28);
-            float: left;
-        }
-        img:nth-child(2),
-        img:nth-child(3) {
-            width: pxTorem(222);
-            height: pxTorem(222);
-            margin-right: pxTorem(28);
-            float: right;
-        }
-        img:nth-child(2) {
-            margin: pxTorem(14) pxTorem(28) pxTorem(12) 0;
+        img {
+            width: pxTorem(220);
+            height: pxTorem(220);
         }
     }
 </style>
 <template>
     <div class='v-subject'>
-        <img v-for='(item,$index) in layout.items' v-lazy='$index==0?item.pic_init_new:item.pic_thumb_new' @click='routerLink(item,layout)'>
+        <img v-for='item in layout.items' v-lazy='item.pic_thumb_new' @click='routerLink(item,layout)'>
     </div>
 </template>
 <script>
