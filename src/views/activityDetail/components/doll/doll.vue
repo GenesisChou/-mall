2cd51bec7c319415176995137e1d003c19f52156
李贤波 <lxb@justtong.com>
2017-05-15 16:15:46 +0800

    娃娃机未中奖弹框bug修复

--- src/views/activityDetail/components/doll/doll.vue
+++ src/views/activityDetail/components/doll/doll.vue
@@ -391,6 +391,7 @@
                                 this.init();
                             }
                         };
+                        this.toggleDialog(this.alert);
                     }
                 }
             },
