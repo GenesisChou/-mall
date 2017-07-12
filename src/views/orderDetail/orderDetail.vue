09d9d950e4982288e90761b06a84d036884be19a
genesischou <genesischou@gmail.com>
2017-07-12 10:53:30 +0800

    订单详情修正

--- src/views/orderDetail/orderDetail.vue
+++ src/views/orderDetail/orderDetail.vue
@@ -273,7 +273,6 @@
                         </section>
                     </template>
                     <v-ticket v-else-if='product_type==1||product_type==6' :type='product_type' :order-detail='order_detail' :product-detail='product_detail'></v-ticket>
-                    <v-ticket  :type='product_type' :order-detail='order_detail' :product-detail='product_detail'></v-ticket>
                     <!--商品为流量充值时 -->
                     <v-recharge v-if='product_type==8' :type='product_type' :get-order-detail='getOrderDetail' :order-detail='order_detail' :product-detail='product_detail'></v-recharge>
                     <!-- 详细说明 -->
