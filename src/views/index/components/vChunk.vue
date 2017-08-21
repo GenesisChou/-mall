fe4d34e5ac774ffbee5c7bd0089226324955cd18
genesischou <genesischou@gmail.com>
2017-08-21 09:51:25 +0800

    魔方

--- src/views/index/components/vChunk.vue
+++ src/views/index/components/vChunk.vue
@@ -9,7 +9,7 @@
 </style>
 <template>
     <div class='v-chunk' :style='layout.height|getHeight'>
-        <div v-for='item in layout.items' :style='item|getCod' @click='routerLink(item,layout)'></div>
+        <div v-for='item in layout.items' :style='getCod(item)' @click='routerLink(item,layout)'></div>
     </div>
 </template>
 <script>
@@ -19,32 +19,21 @@
             layout: Object,
             routerLink: Function,
         },
-        data() {
-            return {
-                // layout: {
-                //     height: 3,
-                //     items: [{
-                //         cod: {
-                //             start: [0, 0],
-                //             end: [1, 2]
-                //         },
-                //         pic_banner_new: 'http://static.justtong.com/uploads/images/goods/20170719/181604596f31642f2e3_250.jpg'
-                //     }, {
-                //         cod: {
-                //             start: [2, 1],
-                //             end: [4, 3]
-                //         },
-                //         pic_banner_new: 'http://static.justtong.com/uploads/images/goods/20170719/181635596f3183db864_250.jpg'
-                //     }]
-                // }
-            };
-        },
         filters: {
+            getHeight(value) {
+                const height = utils.pxTorem(value * 150);
+                return {
+                    height
+                };
+            }
+        },
+        methods: {
             getCod(item) {
-                const start = item.cod.start,
-                    end = item.cod.end,
+                const cod = JSON.parse(item.cod),
+                    start = cod.start,
+                    end = cod.end,
                     left = utils.pxTorem((Math.abs(start[0])) * 150),
-                    top = utils.pxTorem((Math.abs(start[1])) * 150),
+                    top = utils.pxTorem((Math.abs(start[1] - Math.floor(this.layout.start_point) || 0)) * 150),
                     width = utils.pxTorem((Math.abs(end[0] - start[0])) * 150),
                     height = utils.pxTorem((Math.abs(end[1] - start[1])) * 150);
                 return {
@@ -55,13 +44,7 @@
                     height,
                     background: `url('${item.pic_banner_new}') no-repeat 100% 100%/100% 100%`
                 };
-            },
-            getHeight(value) {
-                const height = utils.pxTorem(value * 150);
-                return {
-                    height
-                };
             }
-        },
+        }
     };
 </script>
