9a718eae64d2b48df6a7bf2385ad49f40a09e802
genesischou <genesischou@gmail.com>
2017-09-14 15:05:53 +0800

    no message

--- dist/js/basic.js
+++ dist/js/basic.js
@@ -1,1 +1,1 @@
-webpackJsonp([1],{176:function(e,t,n){"use strict";document.documentElement.style.fontSize=document.documentElement.clientWidth/7.5+"px"}},[176]);
+webpackJsonp([1],{177:function(e,t,n){"use strict";document.documentElement.style.fontSize=document.documentElement.clientWidth/7.5+"px"}},[177]);
--- src/views/games/children/evprotection/children/main/main.vue
+++ src/views/games/children/evprotection/children/main/main.vue
@@ -52,7 +52,9 @@
 
 <template>
   <div class='main'>
-    <iframe class='audio' frameborder="no" border="0" marginwidth="0" marginheight="0" width=298 height=52 src="//music.163.com/outchain/player?type=2&id=758330&auto=1&height=32"></iframe>
+    <video ref='audio' loop='loop' name="media">
+      <source src="http://other.web.rr01.sycdn.kuwo.cn/resource/n1/71/16/2501779746.mp3" type="audio/mpeg">
+    </video>
     <rubbish v-for='item in rubbish' :rubbish.sync='item' :clear-rubbish='clearRubbish' :speed='speed' :key='item.key' :status='status'></rubbish>
     <v-time :time='time' :left-time='left_time'></v-time>
     <score>
@@ -84,6 +86,7 @@
   import guide from './components/guide'
   import redpacket from './components/redpacket'
   import vShareGuide from 'components/vShareGuide'
+  import wx from 'weixin-js-sdk';
   export default {
     components: {
       vTime,
@@ -141,6 +144,11 @@
       if (this.first_enter === true) {
         this.guide_show = true;
       }
+    },
+    mounted() {
+      wx.ready(() => {
+        this.$refs.audio.play();
+      })
     },
     activated() {
       if (this.init === true && this.first_enter === false) {
